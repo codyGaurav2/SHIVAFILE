e0597b694efc0e5e54762b00cc5f652153d64876
Gauravbfile <gkprasad264@gmail.com>
2025-04-13 13:58:46 +0530

    change done

---
+++ index.js
@@ -0,0 +1,99 @@
+// const person = {
+//     name: "Sachin",
+//     age :19,
+//     isAdult : true,
+
+// }
+
+// console.log(person);
+// console.log(typeof person);
+// console.log(person.name);
+// console.log(person["age"]);
+
+// const coll = new Object();
+
+// coll.car = "bmw";
+// coll.bike = "hero";
+
+// console.log(coll);
+
+// function carName(fName,year){
+//     this.fName = fName;
+//     this.year = year;
+
+// }
+
+// const user2 = new carName("alto",2021);
+// console.log(user2);
+
+// user2.color ="black";
+
+// delete user2.fName;
+
+// console.log(user2);
+
+
+// const animal = {
+//     type:"Mammal",
+//     livesIn : "Jungle"
+// }
+
+// const dog = Object.create(animal);
+// dog.breed = "Labra";
+// dog.age = 18;
+
+// console.log(animal);
+// console.log(dog);
+// console.log(dog.breed);
+// console.log(dog.age);
+// console.log(animal.breed);
+// console.log(animal.age);
+// console.log(dog.type);
+// console.log(dog.livesIn);
+
+
+// function createRectangle(len,bre){
+
+//     return rectangle ={
+//         length : len,
+//         breadth : bre,
+
+  
+//     }
+
+// }
+
+// let rectangleObj1 = createRectangle(5,4);
+// console.log( rectangleObj1.hasownproperty("age"));
+
+// let arr = new Array("apple","mango","Papaya");
+// console.log(arr.length);
+
+// arr.push("purple");
+// arr.unshift("red");
+// arr.splice(2,0,"apple");
+
+
+// arr.shift();
+// arr.splice(3,1);
+// console.log(arr);
+
+// let arr = [1,2,3,4];
+
+// let arr2 = [5,6,7,8];
+
+// let combine = arr.concat(arr2);
+// console.log(combine);
+
+// let sliced= combine.slice(2,4);
+// console.log(sliced);
+
+let i = 100
+do{
+    console.log("hellooo");
+
+    i = i-5
+}
+
+while(i<=100);
+
---
+++ script.js
@@ -0,0 +1,10 @@
+
+let i = 100
+do{
+    console.log("hellooo");
+
+    i = i-5
+}
+
+while(i<=100);
+
